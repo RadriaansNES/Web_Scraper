71d5f1ca4146e95ebf01655404682cbfa0d50588
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-05-03 11:23:49 -0400

    Final version.

--- WebScraper.py
+++ WebScraper.py
@@ -18,12 +18,19 @@ for job_element in job_elements:
 
 ## Isolate for specific jobs (lambda looks at the text of each h2 element, converting it to lowercase and then checking if the substring python is present)
 pyth_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
+pyth_jobs_elem = [h2_element.parent.parent.parent for h2_element in pyth_jobs]
 
-for job_element in pyth_jobs:
+
+for job_element in pyth_jobs_elem:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
     print(title_element.text.strip())
     print(company_element.text.strip())
     print(location_element.text.strip())
+    
+    ## section to extract job posting url 
+    links = job_element.find_all("a")
+    link_url = job_element.find_all("a")[1]["href"]
+    print(f"Apply here: {link_url}\n")
     print()
